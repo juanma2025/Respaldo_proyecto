14c8f0e73e243405adcfaa68eac1706d17de552f
Juan Manuel cabrera <127572806+juanma2025@users.noreply.github.com>
2025-05-29 00:19:18 -0500

    desing

--- Front_Citas_Medicas/src/App.jsx
+++ Front_Citas_Medicas/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RegistrarPaciente from './components/PatientRegister';
+import RegistrarMedico from './components/DoctorRegister';
+import IniciarSesion from './components/Login'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="min-h-screen bg-gradient-to-br from-gray-800 via-slate-900 to-gray-950 p-6 flex flex-col items-center justify-center">
+        <h1 className="text-4xl md:text-5xl text-white font-bold text-center mt-9 mb-4 drop-shadow-lg">Plataforma Citas Médica</h1>
+
+        <nav className="flex flex-col md:flex-row justify-center gap-4 mt-6">
+          
+            <button
+            onClick={() => (window.location.href = "auth/login/")}
+            className="flex items-center gap-2 bg-cyan-700 text-white px-6 py-3 rounded-xl hover:bg-cyan-600 transition-all duration-300 shadow-lg hover:scale-105"
+          >
+            Iniciar Sesión 
+          </button>
+
+          <button
+            onClick={() => (window.location.href = "auth/register/patient/")}
+            className="flex items-center gap-2 bg-cyan-700 text-white px-6 py-3 rounded-xl hover:bg-cyan-600 transition-all duration-300 shadow-lg hover:scale-105"
+          >
+            Registrar Paciente
+          </button>
+
+          <button
+            onClick={() => (window.location.href = "auth/register/doctor/")}
+            className="flex items-center gap-2 bg-indigo-700 text-white px-6 py-3 rounded-xl hover:bg-indigo-600 transition-all duration-300 shadow-lg hover:scale-105"
+          >
+            Registrar Médico
+          </button>
+
+
+        </nav>
+        <div className="mt-8 w-full max-w-3xl">
+          <Routes>
+          <Route path="auth/register/patient/" element={<RegistrarPaciente />} />
+          <Route path="auth/register/doctor/" element={<RegistrarMedico />} />
+          <Route path="auth/login/" element={<IniciarSesion />} />
+
+        </Routes>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
